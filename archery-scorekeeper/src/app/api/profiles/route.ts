8c70a8fcfc6196fe0288cceeddaa7a75e0a73a4b
Corey Richardson <richardsoncorey04@gmail.com>
2025-11-21 13:20:34 +0000

    Improved Profile/RecordsSummary entry creation handling for OAuth login/signups

--- archery-scorekeeper/src/app/api/profiles/route.ts
+++ archery-scorekeeper/src/app/api/profiles/route.ts
@@ -5,22 +5,37 @@ export async function POST(request: NextRequest) {
     try {
         const { id, name } = await request.json();
 
-        const user = await prisma.profile.create({
-            data: {
+        if (!id) {
+            return NextResponse.json({ error: "Missing User ID field from request." }, { status: 400 });
+        }
+
+        /* Prisma Client does not have a findOrCreate() query. 
+        You can use upsert() as a workaround. To make upsert() behave like a 
+        findOrCreate() method, provide an empty update parameter to upsert(). */
+        // https://www.prisma.io/docs/orm/prisma-client/queries/crud#update-or-create-records
+
+        const user = await prisma.profile.upsert({
+            where: { id },
+            update: {},
+            create: {
                 id, // From Supabase.auth
                 name,
             },
         });
 
-        const recordsSummary = await prisma.recordsSummary.create({
-            data: {
+        const recordsSummary = await prisma.recordsSummary.upsert({
+            where: { userId: id },
+            update: {},
+            create: {
                 userId: id,
             },
         });
 
         return NextResponse.json({ user, recordsSummary });
+        
     } catch (error: unknown) {
         const message = error instanceof Error ? error.message : "Unknown error";
+        console.error(message);
         return NextResponse.json({ error: message }, { status: 500 });
     }
 }
--- archery-scorekeeper/src/app/auth/callback/page.tsx
+++ archery-scorekeeper/src/app/auth/callback/page.tsx
@@ -1,17 +1,37 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "@/app/utils/supabase/client";
 
 const AuthCallback = () => {
     const router = useRouter();
+    const isProcessed = useRef(false);
 
     useEffect(() => {
         const handleAuth = async () => {
+            if (isProcessed.current) return;
+            isProcessed.current = true;
+
             const { data, error: _error } = await supabase.auth.getSession();
 
             if (data.session) {
+                const user = data.session.user;
+
+                try {
+                    await fetch("/api/profiles", {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json"},
+                        body: JSON.stringify({
+                            id: user.id,
+                            name: user.user_metadata.name,
+                        }),
+                    });
+                }
+                catch (error) {
+                    console.log("Failed to create profile: " + error);
+                }
+
                 router.push("/dashboard");
             } else {
                 router.push("/");
@@ -22,7 +42,7 @@ const AuthCallback = () => {
     }, [ router ]);
 
     return ( 
-        <p>Handling sign-in...</p>
+        <p>Handling sign-in and creating or finding Profile and RecordsSummary entry...</p>
     );
 }
  
