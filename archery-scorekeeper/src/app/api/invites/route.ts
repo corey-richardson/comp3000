3decdece014d745dba92ed5e58daec8311beae18
Corey Richardson <richardsoncorey04@gmail.com>
2025-12-03 15:57:53 +0000

    API: Refactor invites route into more RESTful hierarchal URLs

--- archery-scorekeeper/src/app/api/invites/route.ts
+++ archery-scorekeeper/src/app/api/invites/route.ts
@@ -1,47 +1,7 @@
 import { NextResponse } from "next/server";
 
 import prisma from "@/app/lib/prisma";
-import { getAuthenticatedUser, requireRoleInClub, requireRoleInSharedClubOrOwnership } from "@/app/utils/server-utils";
-
-export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const userId = searchParams.get("userId");
-
-    if (!userId) {
-        return NextResponse.json(
-            { error: "Missing User ID field from query." }, 
-            { status: 400 }
-        );
-    }
-
-    try {
-        const isAllowed = await requireRoleInSharedClubOrOwnership( userId, ["ADMIN"]);
-
-        if (!isAllowed) {
-            return NextResponse.json(
-                { error: "Unauthorised." }, 
-                { status: 401 }
-            );
-        }
-
-        const invites = await prisma.invite.findMany({
-            where: { userId },
-            include: {
-                club: true,
-                invitee: true,
-                inviter: true,
-            },
-        });
-
-        return NextResponse.json(invites, { status: 200 });
-    }
-    catch (error: unknown) {
-        return NextResponse.json(
-            { error: "Internal Server Error: " + error }, 
-            { status: 500 }
-        );
-    }
-}
+import { getAuthenticatedUser, requireRoleInClub } from "@/app/utils/server-utils";
 
 export async function POST(request: Request) {
     try {
