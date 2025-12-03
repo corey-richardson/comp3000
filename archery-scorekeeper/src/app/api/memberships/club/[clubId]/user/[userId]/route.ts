cd8bfc6a8de143549da90d493abdcea1210d6f39
Corey Richardson <richardsoncorey04@gmail.com>
2025-12-03 12:08:27 +0000

    API: Membership DELETE

--- archery-scorekeeper/src/app/api/memberships/club/[clubId]/user/[userId]/route.ts
+++ archery-scorekeeper/src/app/api/memberships/club/[clubId]/user/[userId]/route.ts
@@ -1,36 +1,38 @@
 import { NextResponse } from "next/server";
 
 import prisma from "@/app/lib/prisma";
-import { getAuthenticatedUser, requireRoleInClub } from "@/app/utils/server-utils";
+import { requireRoleInSpecificClubOrOwnership } from "@/app/utils/server-utils";
 
 export async function PATCH(request: Request, { params }: { params: { clubId: string, userId: string } }) {
     const { clubId, userId } = await params;
 
     if (!clubId || !userId) {
-        return NextResponse.json({ error: "Missing Club ID or User ID field from request parameters." }, { status: 400 });
+        return NextResponse.json(
+            { error: "Missing Club ID or User ID field from request parameters." }, 
+            { status: 400 }
+        );
     }
 
     try {
-        const requestor = await getAuthenticatedUser();
-        const isOwner = requestor.id === userId;
-        const isElevated = await requireRoleInClub(clubId, ["ADMIN"]);
-
-        if (!isOwner && !isElevated) {
-            return NextResponse.json({ error: "Unauthorised." }, { status: 401 });
+        const allowed = requireRoleInSpecificClubOrOwnership(userId, clubId, ["ADMIN"]);
+        if (!allowed) {
+            return NextResponse.json(
+                { error: "Unauthorised." }, 
+                { status: 401 }
+            );
         }
 
         const { roles } = await request.json();
         
         const existingMembership = await prisma.membership.findFirst({
-            where: {
-                userId,
-                clubId,
-                ended_at: null,
-            },
+            where: { userId, clubId, ended_at: null },
         });
 
         if (!existingMembership) {
-            return NextResponse.json({ error: "An active club membership could not be found." }, { status: 404 });
+            return NextResponse.json(
+                { error: "An active club membership could not be found." }, 
+                { status: 404 }
+            );
         }
 
         // Prevents orphaning a club
@@ -48,22 +50,103 @@ export async function PATCH(request: Request, { params }: { params: { clubId: st
             });
 
             if (adminCount == 0) {
-                return NextResponse.json({ error: "Cannot remove the last admin from the club." }, { status: 400 });
+                return NextResponse.json(
+                    { error: "Cannot remove the last admin from the club." }, 
+                    { status: 400 }
+                );
             }
         }
         
         const updatedMembership = await prisma.membership.update({
-            where: {
-                id: existingMembership.id,
-            },
-            data: {
-                roles,
-            }
+            where: { id: existingMembership.id },
+            data: { roles }
         });
 
         return NextResponse.json(updatedMembership, { status: 200 });
     }
     catch (error: unknown) {
-        return NextResponse.json({ error: "Internal Server Error: " + error }, { status: 500 });
+        return NextResponse.json(
+            { error: "Internal Server Error: " + error }, 
+            { status: 500 }
+        );
+    }
+}
+
+export async function DELETE(request: Request, { params }: { params: { clubId: string, userId: string } }) {
+    const { clubId, userId } = await params;
+
+    if (!clubId || !userId) {
+        return NextResponse.json(
+            { error: "Missing Club ID or User ID field from request parameters." }, 
+            { status: 400 }
+        );
+    }
+
+    try {
+        const allowed = requireRoleInSpecificClubOrOwnership(userId, clubId, ["ADMIN"]);
+        if (!allowed) {
+            return NextResponse.json(
+                { error: "Unauthorised." }, 
+                { status: 401 }
+            );
+        }
+
+        const membership = await prisma.membership.findFirst({
+            where: { clubId, userId, ended_at: null },
+            include: { profile: true, club: true },
+        });
+
+        if (!membership) {
+            return NextResponse.json(
+                { error: "Membership not found." }, 
+                { status: 404 }
+            );
+        }
+
+        if (membership.clubId !== clubId) {
+            return NextResponse.json(
+                { error: "Membership does not belong to this club." }, 
+                { status: 400 }
+            );
+        }
+
+        if (membership.ended_at) {
+            return NextResponse.json(
+                { error: "Membership has already ended." }, 
+                { status: 400 }
+            );
+        }
+
+        if (membership.roles.includes("ADMIN")) {
+            const remainingAdmins = await prisma.membership.count({
+                where: { 
+                    clubId, 
+                    ended_at: null, 
+                    roles: {
+                        has: "ADMIN",
+                    },
+                    NOT: { userId }, // Exclude self
+                }
+            });
+
+            if (remainingAdmins === 0) {
+                return NextResponse.json(
+                    {error: "You cannot leave as the only admin. Please assign another member to be an admin or delete the club."}, 
+                    { status: 403 });
+            }
+        }
+
+        const updatedMembership = await prisma.membership.update({
+            where: { id: membership.id },
+            data: { ended_at: new Date() },
+        });
+
+        return NextResponse.json(updatedMembership, { status: 200 });
+    }
+    catch (error: unknown) {
+        return NextResponse.json(
+            { error: "Internal Server Error: " + error }, 
+            { status: 500 }
+        );
     }
 }
