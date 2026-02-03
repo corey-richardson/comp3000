e50878a750ecd549e264307e164b4ff2eba3fd6e
Corey Richardson <richardsoncorey04@gmail.com>
2026-02-03 15:09:11 +0000

    Update Flask service to include `/rounds` route to fetch Indoor and Outdoor rounds; update body`.get` to use `roundCodeName` field rather than display name

--- SourceCode/archeryutils-service/app.py
+++ SourceCode/archeryutils-service/app.py
@@ -24,7 +24,7 @@ def calculate_metrics():
     
     try:
         raw_score = data.get("score")
-        round_codename = data.get("roundName")
+        round_codename = data.get("roundCodeName")
         
         bowstyle = classifications.AGB_bowstyles[data.get("bowstyle").upper()]
         sex_category = classifications.AGB_genders[data.get("sex").upper()]
@@ -69,7 +69,64 @@ def calculate_metrics():
         return jsonify({ "error": f"Invalid Enum Value: {str(e)}" }), 400
     except Exception as e:
         return jsonify({ "error": str(e) }), 500
+
+
+@app.route("/rounds", methods=["GET"])
+def get_rounds():
     
+    MISC_INDOOR_EXPLICIT = { "lancaster", "lancaster_triple" }
+    
+    indoor_dict = [
+        load_rounds.AGB_indoor,
+        load_rounds.WA_indoor
+    ]
+    
+    outdoor_dict = [
+        load_rounds.AGB_outdoor_imperial,
+        load_rounds.AGB_outdoor_metric,
+        load_rounds.WA_outdoor,
+        load_rounds.WA_experimental
+    ]
+    
+    indoor_rounds = []
+    outdoor_rounds = []
+    
+    for round in indoor_dict:
+        for code_name, object in round.items():
+            indoor_rounds.append({ 
+                "codename": code_name, 
+                "name": object.name, 
+                "max_score": object.max_score(), 
+                "num_arrows": object.n_arrows 
+            })
+            
+    for round in outdoor_dict:
+        for code_name, object in round.items():
+            outdoor_rounds.append({ 
+                "codename": code_name, 
+                "name": object.name, 
+                "max_score": object.max_score(), 
+                "num_arrows": object.n_arrows 
+            })
+            
+    for code_name, object in load_rounds.misc.items():
+        round_data = { 
+            "codename": code_name, 
+            "name": object.name, 
+            "max_score": object.max_score(),
+            "num_arrows": object.n_arrows 
+        }
+        if code_name in MISC_INDOOR_EXPLICIT:
+            indoor_rounds.append(round_data)
+        else:
+            outdoor_rounds.append(round_data)
+            
+    return jsonify({
+        "INDOOR": sorted(indoor_rounds, key=lambda x: x["name"]),
+        "OUTDOOR": sorted(outdoor_rounds, key=lambda x: x["name"])
+    })
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
     
