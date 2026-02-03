7bbafe5dea6c5c91ef0bca27e4a5f903863e7445
Corey Richardson <richardsoncorey04@gmail.com>
2026-02-03 15:11:49 +0000

    Added a display format flag to choose between Enum value and display value return

--- SourceCode/frontend/src/lib/calculateAgeCategory.js
+++ SourceCode/frontend/src/lib/calculateAgeCategory.js
@@ -10,16 +10,16 @@ const AGE_CAT_U18_YEAR = CURRENT_YEAR - 18;
 const AGE_CAT_U21_YEAR = CURRENT_YEAR - 21;
 const AGE_CAT_O50_YEAR = CURRENT_YEAR - 50;
 
-function calculateAgeCategory(year) {
-    if (year === null) return EnumMap["SENIOR"];
-    if (year > AGE_CAT_U12_YEAR) return EnumMap["UNDER_12"];
-    if (year > AGE_CAT_U14_YEAR) return EnumMap["UNDER_14"];
-    if (year > AGE_CAT_U15_YEAR) return EnumMap["UNDER_15"];
-    if (year > AGE_CAT_U16_YEAR) return EnumMap["UNDER_16"];
-    if (year > AGE_CAT_U18_YEAR) return EnumMap["UNDER_18"];
-    if (year > AGE_CAT_U21_YEAR) return EnumMap["UNDER_21"];
-    if (year <= AGE_CAT_O50_YEAR) return EnumMap["OVER_FIFTY"];
-    return EnumMap["SENIOR"];
+function calculateAgeCategory(year, formattingFlag) {
+    if (year === null) return formattingFlag ? EnumMap["SENIOR"] : "SENIOR";
+    if (year > AGE_CAT_U12_YEAR) return formattingFlag ? EnumMap["UNDER_12"] : "UNDER_12";
+    if (year > AGE_CAT_U14_YEAR) return formattingFlag ? EnumMap["UNDER_14"] : "UNDER_14";
+    if (year > AGE_CAT_U15_YEAR) return formattingFlag ? EnumMap["UNDER_15"] : "UNDER_15";
+    if (year > AGE_CAT_U16_YEAR) return formattingFlag ? EnumMap["UNDER_16"] : "UNDER_16";
+    if (year > AGE_CAT_U18_YEAR) return formattingFlag ? EnumMap["UNDER_18"] : "UNDER_18";
+    if (year > AGE_CAT_U21_YEAR) return formattingFlag ? EnumMap["UNDER_21"] : "UNDER_21";
+    if (year <= AGE_CAT_O50_YEAR) return formattingFlag ? EnumMap["OVER_FIFTY"] : "OVER_FIFTY";
+    return formattingFlag ? EnumMap["SENIOR"] : "SENIOR";
 }
 
 export default calculateAgeCategory;
