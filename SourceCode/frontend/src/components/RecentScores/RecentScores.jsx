2eda5e82ffc308fb2e59d1f7106fef049aaf3460
Corey Richardson <richardsoncorey04@gmail.com>
2026-04-05 14:56:58 +0100

    Move API call for Scores and Records Summary into parent Dashboard component, pass data as props to child components; reduced number of API calls

--- SourceCode/frontend/src/components/RecentScores/RecentScores.jsx
+++ SourceCode/frontend/src/components/RecentScores/RecentScores.jsx
@@ -1,49 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import RecentScoreItem from "./RecentScoreItem";
-import { useApi } from "../../hooks/useApi";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import dashboardStyles from "../../styles/Dashboard.module.css";
 
-const RecentScores = () => {
-    const { user, authIsReady } = useAuthContext();
-    const { makeApiCall } = useApi();
-
-    const [ scores, setScores ] = useState([]);
-    const [ isLoading, setIsLoading ] = useState(true);
-    const [ error, setError ] = useState(null);
-
-    useEffect(() => {
-        const fetchLatestScores = async () => {
-            if (!authIsReady || !user?.id) return;
-
-            setError(null);
-            setIsLoading(true);
-
-            try {
-                const response = await makeApiCall(`/api/scores/user/${user.id}?limit=3`);
-                if (!response) return; // 401
-
-                const data = await response.json();
-                if (response.ok) {
-                    setScores(data.scores);
-                }
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-
-        fetchLatestScores();
-    }, [user.id, user.token, makeApiCall, authIsReady]);
+const RecentScores = ({ scores, isLoading, error }) => {
 
     return (
         <div className={dashboardStyles.dashboardContainer}>
             <h2>Recent Scores.</h2>
 
-            { scores && scores.length > 0 && (
+            { !isLoading && scores && scores.length > 0 && (
                 <>
                     { scores.map(score => (
                         <RecentScoreItem score={score} key={score.id} />
--- SourceCode/frontend/src/pages/Dashboard.jsx
+++ SourceCode/frontend/src/pages/Dashboard.jsx
@@ -1,22 +1,68 @@
 
+import { useEffect, useState } from "react";
+
 import ClubCards from "../components/ClubCards/ClubCards.jsx";
 import CurrentClassificationsAndHandicaps from "../components/CurrentClAndHc/CurrentClAndHc.jsx";
 import DetailsForm from "../components/DetailsForm/DetailsForm.jsx";
 import EmergencyContacts from "../components/EmergencyContacts/EmergencyContacts.jsx";
 import RecentScores from "../components/RecentScores/RecentScores.jsx";
+import { useApi } from "../hooks/useApi";
 import { useAuthContext } from "../hooks/useAuthContext";
 import dashboardStyles from "../styles/Dashboard.module.css";
 
 const Dashboard = () => {
-    const { user } = useAuthContext();
+    const { user, authIsReady } = useAuthContext();
+    const { makeApiCall } = useApi();
+
     const userId = user?.id;
+
+    const [ scores, setScores ] = useState([]);
+    const [ summary, setSummary ] = useState(null);
+
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ error, setError ] = useState(null);
+
+    useEffect(() => {
+        const fetchLatestScoresAndRecordsSummary = async () => {
+            if (!authIsReady || !user?.id) return;
+
+            setError(null);
+            setIsLoading(true);
+
+            try {
+                const response = await makeApiCall(`/api/scores/user/${user.id}?limit=3`);
+                if (!response) return; // 401
+
+                const data = await response.json();
+                if (response.ok) {
+                    setScores(data.scores);
+                    setSummary(data.summary);
+                }
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchLatestScoresAndRecordsSummary();
+    }, [user.id, user.token, makeApiCall, authIsReady]);
 
     return (
         <div>
 
             <div className={dashboardStyles.dashboardGrid}>
-                <RecentScores userId={userId} />
-                <CurrentClassificationsAndHandicaps userId={userId} />
+                <RecentScores
+                    scores={scores}
+                    isLoading={isLoading}
+                    error={error}
+                />
+
+                <CurrentClassificationsAndHandicaps
+                    summary={summary}
+                    isLoading={isLoading}
+                    error={error}
+                />
             </div>
 
             <DetailsForm userId={userId} />
