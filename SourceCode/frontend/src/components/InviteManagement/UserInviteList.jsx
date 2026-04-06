39827652874977028af55000348b43e42377f8bb
Corey Richardson <richardsoncorey04@gmail.com>
2026-04-06 18:33:33 +0100

    Dummy `UserInviteList` and `UserInviteListItem` components; created base `Card` style module to be used by `ScoreItem` and `ClubItem` (etc) components TODO

--- SourceCode/frontend/src/components/InviteManagement/UserInviteList.jsx
+++ SourceCode/frontend/src/components/InviteManagement/UserInviteList.jsx
@@ -1,15 +1,110 @@
+import { Check, X } from "lucide-react";
 import { useState } from "react";
 
 import { useApi } from "../../hooks/useApi";
+import badgeStyles from "../../styles/BadgeGroups.module.css";
+import cardStyles from "../../styles/Card.module.css";
+import DeleteOverlay from "../DeleteOverlay/DeleteOverlay";
 
-const UserInviteList = ({ invites, isLoading, error }) => {
+const UserInviteListItem = ({ invite, onRemove, setError }) => {
+    const { makeApiCall } = useApi();
+
+    const [isConfirming, setIsConfirming] = useState(false);
+    const [isActioning, setIsActioning] = useState(false);
+
+    const handleAction = async (inviteId, action) => {
+        setIsActioning(true);
+
+        try {
+            console.log(inviteId, action);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setIsActioning(false);
+            setIsConfirming(false);
+        }
+    };
+
     return (
-        <>
-            <p>User Invite List</p>
-            <p>Invites: {invites.length}</p>
-            <p>Loading: {isLoading ? "true" : "false"}</p>
-            <p>Error: {error}</p>
-        </>
+        <div className={cardStyles.cardItem}>
+            <div className={cardStyles.topRow}>
+                <span className={cardStyles.date}>
+                    Invited on { new Date(invite.created_at).toLocaleDateString() }
+                </span>
+            </div>
+
+            <div className={cardStyles.mainInfo}>
+                <strong>{ invite.club.name }</strong>
+
+                <div className={badgeStyles.group}>
+                    <button
+                        onClick={() => handleAction(invite.id, "accept")}
+                        className={badgeStyles.badge}
+                        title={ "Accept Invite" }
+                        disabled={isActioning || isConfirming}
+                    >
+                        <Check />
+                    </button>
+
+                    <button
+                        onClick={() => setIsConfirming(true)}
+                        className={badgeStyles.badge}
+                        title={ "Decline Invite" }
+                        disabled={isActioning || isConfirming}
+                    >
+                        <X />
+                    </button>
+                </div>
+            </div>
+
+            <div className={cardStyles.footerRow}>
+                { invite.club.name } have invited you to join their club.
+            </div>
+
+            {isConfirming && (
+                <DeleteOverlay
+                    message={`Decline invite from ${ invite.club.name }?`}
+                    onConfirm={() => handleAction("decline")}
+                    onCancel={() => setIsConfirming(false)}
+                    isPending={isActioning}
+                    confirmButtonText="Decline"
+                    confirmButtonTextAction="Declining"
+                    type="row"
+                />
+            )}
+        </div>
+    );
+};
+
+const UserInviteList = ({
+    invites, setInvites,
+    totalCount, setTotalCount,
+    isLoading,
+    error, setError
+}) => {
+
+    const handleRemove = (id) => {
+        setInvites(prev => prev.filter(invite => invite.id !== id));
+        setTotalCount(prev => prev - 1);
+    };
+
+    if (isLoading) return <p className="small centred">Loading invites...</p>;
+    if (error) return <p className="error-message">{error}</p>;
+    if (!isLoading && invites.length === 0) return <p className="small centred">No pending invites.</p>;
+
+    return (
+        <div>
+            {invites.map((invite) => (
+                <UserInviteListItem
+                    key={invite.id}
+                    invite={invite}
+                    onRemove={handleRemove}
+                    setError={setError}
+                />
+            ))}
+
+            <p className="small centred">Displaying { invites.length } of { totalCount } invites.</p>
+        </div>
     );
 };
 
--- SourceCode/frontend/src/pages/Members/Invites.jsx
+++ SourceCode/frontend/src/pages/Members/Invites.jsx
@@ -3,12 +3,50 @@ import { useEffect, useState } from "react";
 
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import UserInviteList from "../../components/InviteManagement/UserInviteList";
+import Pagination from "../../components/Pagination/Pagination";
+import { useApi } from "../../hooks/useApi";
+import { usePagination } from "../../hooks/usePagination";
 
 const MemberInvites = () => {
+    const { makeApiCall } = useApi();
+
+    const paginationProps = usePagination();
+    const {
+        currentPage, setTotalPages,
+        totalCount, setTotalCount
+    } = paginationProps;
+
+    const invitesPerPage = 10;
 
     const [ invites, setInvites ] = useState([]);
     const [ isLoading, setIsLoading ] = useState(true);
     const [ error, setError ] = useState(null);
+
+    useEffect(() => {
+        const fetchInvites = async () => {
+            setError(null);
+            setIsLoading(true);
+
+            try {
+                const response = await makeApiCall(`/api/invites/my-invites?page=${currentPage}&limit=${invitesPerPage}`);
+                if (!response) return; // 401
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setInvites(data.invites);
+
+                    setTotalPages(data.pagination.totalPages);
+                    setTotalCount(data.pagination.totalCount);
+                }
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchInvites();
+    }, [ makeApiCall, currentPage, setTotalPages, setTotalCount ]);
 
     return (
         <div className="content">
@@ -17,9 +55,15 @@ const MemberInvites = () => {
             <h2>Invites</h2>
             <UserInviteList
                 invites={invites}
+                setInvites={setInvites}
+                totalCount={totalCount}
+                setTotalCount={setTotalCount}
                 isLoading={isLoading}
                 error={error}
+                setError={setError}
             />
+
+            <Pagination paginationProps={paginationProps} />
         </div>
     );
 };
