eff637bfd30646a678a6fa41800af9817fc26c78
Corey Richardson <richardsoncorey04@gmail.com>
2026-04-24 16:52:55 +0100

    Added real text to Landing Page, replacing lorem ipsum placeholders

--- SourceCode/frontend/src/pages/LandingPage.jsx
+++ SourceCode/frontend/src/pages/LandingPage.jsx
@@ -16,13 +16,17 @@ export default function LandingPage() {
                 <p className="bold">Welcome to the Archery Club Management Tool!</p>
 
                 <p>
-                    Nam vestibulum tempus velit. Sed ipsum lorem, ultrices et posuere pharetra, posuere sit amet sapien. Maecenas gravida vestibulum ex, pharetra auctor risus egestas quis. Etiam sodales velit at posuere pellentesque. Etiam quam mi, viverra ut neque eget, faucibus tincidunt mi. Pellentesque quis neque quis metus imperdiet gravida pretium ac sem. Mauris in dictum eros. Vestibulum sit amet tincidunt mauris. Suspendisse semper auctor dolor, sit amet pharetra odio efficitur non. Aenean ornare posuere nisl, in viverra ligula porta imperdiet. Nam laoreet rhoncus magna facilisis placerat. Nam et porttitor justo, et condimentum tellus.
+                    This website is designed to help archery clubs manage their members and score records efficiently and securely. Whether you are a club administrator, records officer, or a regular member, you will find tools tailored to your role:
                 </p>
+                <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+                    <b>Administrators</b> can manage club details, invite new members, assign roles, and oversee all club activities.<br />
+                    <b>Captains and Tournament Officers</b> have read-only access to club and member information to assist with the management of emergency contact information and competiton entries.<br />
+                    <b>Records Officers</b> have access to member scores, can update records, and help maintain the club&apos;s competitive history.<br />
+                    <b>Coaches</b> can view the scores and performance journals of archers part of their club.<br />
+                    <b>Members</b> can view their personal details, submit scores, and track their progress and performances.<br />
+                </div>
                 <p>
-                    Ut posuere vestibulum ex, vel pharetra augue luctus nec. Nam nibh nisl, accumsan sed laoreet et, convallis at est. Praesent ornare ligula urna, a blandit tortor cursus et. Sed metus diam, scelerisque id vehicula sit amet, auctor at urna. Donec arcu justo, tincidunt non eleifend id, eleifend eu libero. Cras scelerisque, neque sit amet sollicitudin pellentesque, metus turpis tempus lacus, a porta purus lectus at turpis. Nam fringilla urna et suscipit dapibus. Fusce aliquet libero metus, at facilisis tellus aliquam quis. Pellentesque neque metus, sodales ac turpis at, lacinia tempus neque. Proin sodales ligula et blandit vestibulum. Praesent consectetur hendrerit sem, eu volutpat risus laoreet a. Donec quis varius quam. Etiam sodales ipsum vel orci mollis mattis.
-                </p>
-                <p>
-                    Nulla imperdiet, arcu eget tristique laoreet, tellus nisl hendrerit enim, ultrices hendrerit dolor quam vel purus. Suspendisse sapien tellus, tincidunt vitae sapien ut, ultrices blandit diam. Sed nisi arcu, tristique ac varius eget, dignissim ut orci. Aenean ut tincidunt odio. In placerat feugiat magna, nec gravida quam maximus id. Maecenas non sapien efficitur, molestie quam in, hendrerit risus. Praesent porttitor dictum tortor, id tristique velit feugiat eget. Pellentesque a erat id nibh fringilla pellentesque. Sed turpis arcu, lacinia et mauris vel, ultricies condimentum diam. Integer aliquam tellus sit amet turpis gravida, non commodo eros aliquam. Nam risus ipsum, pellentesque eu aliquam mattis, vulputate ac magna. Curabitur tincidunt iaculis nisl nec tristique. Etiam vel lacinia ligula, vel dictum metus. Cras in magna commodo, ullamcorper magna sed, efficitur mauris. Mauris velit nulla, faucibus et ex eget, iaculis lobortis ante. Aliquam erat volutpat.
+                    This tool is free to use and is covered by a <a href="https://github.com/corey-richardson/comp3000/blob/main/LICENSE.txt" target="_blank" rel="noreferrer">GNU General Public License v3.0</a>. This means you are free to use, modify, and share the software, even for commercial purposes, as long as any distributed versions or derivatives remain open-source and are also licensed under the GPL v3.0. You must provide attribution and include the original license when redistributing or publishing changes. Proprietary use or relicensing is not permitted.
                 </p>
 
             </div>
